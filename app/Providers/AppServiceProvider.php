deb7aa079c7d2aecb8eac9b7a5090fd3db39e43b
pratishma09 <velv9tkiss3s@gmail.com>
2025-04-25 13:26:55 +0545

    add(scramble documentation)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,6 +11,9 @@ use App\Repositories\Contracts\BorrowBookRepositoryInterface;
 use App\Services\UserService;
 use App\Services\BookService;
 use App\Services\BorrowBookService;
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -41,5 +44,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Scramble::configure()
+        ->withDocumentTransformers(function (OpenApi $openApi) {
+            $openApi->secure(
+                SecurityScheme::http('bearer')
+            );
+        });
     }
 }
